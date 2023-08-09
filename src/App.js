661944cb3f50962834563fbfeb11aa6c5b3d499d
itsdianaa21 <dianamenendez17@gmail.com>
2023-08-09 18:59:29 -0500

    created dictionary js and css files
    imported files, created dictionary files and functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
@@ -7,6 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo"></img>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">
+        <small>Coded by SheCodes</small>
+      </footer>
     </div>
   );
 }
