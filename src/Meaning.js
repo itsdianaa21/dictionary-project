b035b685407058000ff473bc3c435477fa4bfc74
itsdianaa21 <dianamenendez17@gmail.com>
2023-08-19 19:12:30 -0500

    f

--- src/Meaning.js
+++ src/Meaning.js
@@ -7,7 +7,6 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-        </p>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
